8a92550c6374321eff83576c90583731a20e72a3
mehTro20 <justmondlisok@gmail.com>
2022-06-03 11:19:09 +0200

    adding and subtracting functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,125 @@
-import { useState } from 'react';
-import './App.css';
+import "./App.css";
 
-function App() {
-  const [changeColor, setChangeColor] = useState(false)
+import React, { Component } from "react";
 
+// var colors = ['rgb(' + Math.round(Math.random() * 255) + ',' +
+// Math.round(Math.random() * 255) + ',' +
+// Math.round(Math.random() * 255) + ')'];
+// var random_color = colors[Math.floor(Math.random() * colors.length)];
 
-  const handleChangeColor = () => {
-    setChangeColor(true)
-    // console.log('color is changed')
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+      count: 0,
+    };
+    this.changeColor = this.changeColor.bind(this);
+    this.add = this.add.bind(this);
+    this.minus = this.minus.bind(this);
+  }
+  changeColor() {
+    this.setState({
+      display: !this.state.display,
+    });
+  }
+  add() {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  }
+  minus() {
+    if (this.state.count === 0) {
+      this.setState((state) => ({
+        count: 0,
+      }));
+    } else {
+      this.setState((state) => ({
+        count: state.count - 1,
+      }));
+    }
+  }
+
+  render() {
+    if (this.state.display) {
+      return (
+        <div>
+          <div
+            className="items-div"
+            style={{
+              backgroundColor:
+                "rgb(" +
+                Math.round(Math.random() * 255) +
+                "," +
+                Math.round(Math.random() * 255) +
+                "," +
+                Math.round(Math.random() * 255) +
+                ")",
+              height: "100vh",
+            }}
+          >
+            <div className="countdiv">
+              <h1>Count {this.state.count}</h1>
+            </div>
+            <button onClick={this.minus}>-</button>
+            <button onClick={this.changeColor}>Change Color</button>
+            <button onClick={this.add}>+</button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div
+            className="items-div"
+            style={{
+              backgroundColor:
+                "rgb(" +
+                Math.round(Math.random() * 255) +
+                "," +
+                Math.round(Math.random() * 255) +
+                "," +
+                Math.round(Math.random() * 255) +
+                ")",
+              height: "100vh",
+            }}
+          >
+            <div className="countdiv">
+              <h1>Count: {this.state.count}</h1>
+            </div>
+            <button onClick={this.minus}>-</button>
+            <button onClick={this.changeColor}>Change Color</button>
+            <button onClick={this.add}>+</button>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
-  return (
-    <div className="App">
-{!changeColor ? <h1 style={{color: 'red'}}>Mondli</h1> : <h1>Tumi</h1>}
-      <button onClick={handleChangeColor} >Change color</button>
-    </div>
-  );
-}
+// export default App;
+// function App() {
+//   const [changeColor, setChangeColor] = useState(false)
 
-export default App; 
+//   const handleChangeColor = () => {
+//     setChangeColor(true)
+//     // console.log('color is changed')
+// }
+
+//   render (){
+//     <div className="App">
+// {()=> {
+//   if("fgdfgdf"){
+// return "dfgdfg"
+//   }else if("ghjgh"){
+//     return "dfgdfg"
+//   }else if("ghfgh"){
+//     return "dfgdfg"
+//   }else{
+//     return "dfgdfg"
+//   }
+// }}
+//       <button onClick={handleChangeColor} >Change color</button>
+//     </div>
+//     }
+// }
